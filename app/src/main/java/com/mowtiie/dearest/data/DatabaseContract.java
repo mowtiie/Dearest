4026cb1a7610f723f1fc5f9a66bab184c9868b99
mowtiie <mowtiie.dev@gmail.com>
2026-07-09 00:01:50 +0800

    Created database contract class

---
+++ app/src/main/java/com/mowtiie/dearest/data/DatabaseContract.java
@@ -0,0 +1,34 @@
+package com.mowtiie.dearest.data;
+
+import android.provider.BaseColumns;
+
+public final class DatabaseContract {
+
+    private DatabaseContract() {
+
+    }
+
+    public static final class Notebooks implements BaseColumns {
+        public static final String TABLE = "notebooks";
+        public static final String COL_UUID = "uuid";
+        public static final String COL_NAME = "name";
+        public static final String COL_POSITION = "position";
+        public static final String COL_CREATED  = "created_at";
+    }
+
+    public static final class Entries implements BaseColumns {
+        public static final String TABLE = "entries";
+        public static final String COL_UUID = "uuid";
+        public static final String COL_NOTEBOOK = "notebook_uuid";
+        public static final String COL_TITLE = "title";
+        public static final String COL_BODY = "body";
+        public static final String COL_CREATED = "created_at";
+        public static final String COL_UPDATED = "updated_at";
+    }
+
+    public static final class EntriesFts {
+        public static final String TABLE = "entries_fts";
+        public static final String COL_TITLE = "title";
+        public static final String COL_BODY  = "body";
+    }
+}
