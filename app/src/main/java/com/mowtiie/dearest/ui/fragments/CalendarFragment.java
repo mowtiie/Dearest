4c17e32b8bb2bb89a642496064a58862cd6ceb15
mowtiie <mowtiie.dev@gmail.com>
2026-07-09 20:22:28 +0800

    Created calendar fragment

---
+++ app/src/main/java/com/mowtiie/dearest/ui/fragments/CalendarFragment.java
@@ -0,0 +1,23 @@
+package com.mowtiie.dearest.ui.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.mowtiie.dearest.R;
+
+public class CalendarFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_placeholder, container, false);
+        ((TextView) v.findViewById(R.id.placeholder_text)).setText(R.string.placeholder_calendar);
+        return v;
+    }
+}
