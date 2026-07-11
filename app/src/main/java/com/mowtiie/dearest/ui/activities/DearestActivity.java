ccef0cf059d6ca3ee017527d585282b265d7a859
mowtiie <mowtiie.dev@gmail.com>
2026-07-11 19:38:11 +0800

    Added edge-to-edge for base dearest activity

--- app/src/main/java/com/mowtiie/dearest/ui/activities/DearestActivity.java
+++ app/src/main/java/com/mowtiie/dearest/ui/activities/DearestActivity.java
@@ -3,6 +3,7 @@ package com.mowtiie.dearest.ui.activities;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,7 @@ public abstract class DearestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_SECURE,
                 WindowManager.LayoutParams.FLAG_SECURE
